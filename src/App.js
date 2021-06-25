c1b5ebcfd6919cf35ec911c7fb9e7020777be546
Jen Lim <jennifer.lim07@gmail.com>
2021-06-25 08:56:51 -0700

    refactor and removed print statements

--- src/App.js
+++ src/App.js
@@ -74,27 +74,18 @@ const App = () => {
     //    all three squares have the same value.
 
     for (let i = 0; i < 3; i++) {
-      if (squares[i][0].value !== '' && squares[i][1].value !== '') {
-        if (squares[i][0].value === squares[i][1].value && squares[i][0].value === squares[i][2].value) {
-            console.log('i 0 ' + squares[i][0].value)
+        if (squares[i][0].value === squares[i][1].value && squares[i][1].value === squares[i][2].value && squares[i][1].value !== '') {
             return squares[i][0].value;
         }
-      }
-      if (squares[0][i].value !== '' && squares[1][i].value !== '') {
-        if (squares[0][i].value === squares[1][i].value && squares[0][i].value === squares[2][i].value) {
-          console.log('0 i ' + squares[0][i].value)  
+        if (squares[0][i].value === squares[1][i].value && squares[1][i].value === squares[2][i].value && squares[1][i].value !== '') {
           return squares[0][i].value;
-        } 
       }
-
     }
 
     if (squares[1][1].value !== '') {
       if (squares[0][0].value === squares[1][1].value && squares[0][0].value === squares[2][2].value) {
-        console.log('0 0 ' + squares[0][0].value)
         return squares[0][0].value;
       } else if (squares[0][2].value === squares[1][1].value && squares[0][2].value === squares[2][0].value) {
-        console.log('0 2 ' + squares[0][2].value)
         return squares[0][2].value;
       }
     }
